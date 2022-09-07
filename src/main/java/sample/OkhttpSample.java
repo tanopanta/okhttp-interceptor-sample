daabfc6d61ec89e135c228d4b874b19e9b95e1ab
tanopanta <25007602+tanopanta@users.noreply.github.com>
2022-09-08 00:44:38 +0900

    add okhttp min impl

---
+++ src/main/java/sample/OkhttpSample.java
@@ -0,0 +1,23 @@
+package sample;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class OkhttpSample {
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+            .url("https://httpbin.org/get")
+            .build();
+        
+        try {
+            Response response =  client.newCall(request).execute();
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            // TODO: handle exception
+        }
+    }
+}
